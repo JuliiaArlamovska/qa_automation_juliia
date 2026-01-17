b2aec79a61b3129fab6fff382e6eb1831d92423a
Juliia Arlamovska <yulchuk.if@gmail.com>
2026-01-17 21:48:23 +0200

    primitive data types home work

---
+++ jsBasics/task4.3.js
@@ -0,0 +1,4 @@
+let radius = 9.9;
+let height = 33;
+let cylinderVolume = Math.PI * radius ** 2 * height;// V = πr²h
+console.log((cylinderVolume).toFixed(2));// volume of a cylinder rounded to 2 decimal placesnode
