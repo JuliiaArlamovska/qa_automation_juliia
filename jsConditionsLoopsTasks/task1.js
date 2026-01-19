1a7465b9665f374a78c29346905f0e3cebe6af0a
JuliiaArlamovska <yulchuk.if@gmail.com>
2026-01-19 20:40:43 +0200

    Update jsConditionsLoopsTasks/task1.js
    Co-authored-by: Kwazimodd <55921352+Kwazimodd@users.noreply.github.com>

--- jsConditionsLoopsTasks/task1.js
+++ jsConditionsLoopsTasks/task1.js
@@ -3,7 +3,7 @@ if (averageGrade < 60) {
     console.log("not satisfied")
 }else if (averageGrade >= 60 && averageGrade <= 70)
      {console.log("satisfied")
-}else if (averageGrade > 71 && averageGrade <= 80) {
+}else if (averageGrade >= 71 && averageGrade <= 80) {
     console.log("good")
 }else if (averageGrade > 81 && averageGrade <= 90) {
     console.log("very good")  
