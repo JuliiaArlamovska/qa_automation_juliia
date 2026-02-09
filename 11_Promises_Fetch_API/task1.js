b8bb5116a9b7adedd611edb009be454f11fe5c29
Juliia Arlamovska <yulchuk.if@gmail.com>
2026-02-09 20:54:44 +0200

    fixed 1 task

--- 11_Promises_Fetch_API/task1.js
+++ 11_Promises_Fetch_API/task1.js
@@ -1,11 +1,10 @@
-// Task 1: Using setTimeout to display messages after a delay
-function showMessage (message, sec) {
+function delayText(text, ms) { //Function and it arguments
+  setTimeout(() => {          
+    console.log(text);
+  }, 
+  ms);
+}
 
-// This function will be called after a specified delay to display a message
-    console.log(`${message}, ${sec}`)
-};
-// Schedule the showMessage function to be called after 3 seconds and 5 seconds with different messages
-setTimeout(showMessage, 3000, 'Seconds', '3'); 
-setTimeout(showMessage, 5000, 'Seconds', '5');
-// Task 2: Using setInterval to display messages at regular intervals
-setInterval(showMessage,4000, 'Seconds', '4');
+// Приклад виклику:
+delayText("Text appear in 5 sec", 5000);
+delayText("Text appear in 10 sec", 10000);
